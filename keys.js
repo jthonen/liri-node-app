a50123421b7a5c82b83150671e1154910d662720
jthonen <thonen.jacob@gmail.com>
2018-10-25 11:32:33 -0700

    update

--- keys.js
+++ keys.js
@@ -3,5 +3,7 @@ console.log('this is loaded');
 exports.spotify - {
     id: ProcessingInstruction.env.SPOTIFY_ID,
     secret: ProcessingInstruction.env.SPOTIFY_SECRET
-}
+};
+
+
 
--- liri.js
+++ liri.js
@@ -1,3 +1,24 @@
+// Require dotenv for API keys... 
 require("dotenv").config();
 
+//Let's user know what commands are before anything happens... 
+console.log("Options: concert-this | spotify-this-song | movie-this | do-what-it-says");
 var spotify = new spotify(keys.spotify);
+var input = process.argv;
+
+// Switch statement to control what function gets run and when...
+switch (true) {
+    case (input === "concert-this"):
+        //run function callback bands in town
+        console.log("Running Concert Query>>>");
+    case (input === " spotify-this-song"):
+        //run function callback spotify
+        console.log("Running Spotify Query>>>");
+    case (input === "movie-this"):
+        //run funciton callback omdb
+        console.log("Running OMDB Query>>>");
+    case (input === "do-what-it-says"):
+        //run function callback ???
+        console.log("I did the thing>>>");
+}
+
